33036e8ee7c0e016343b1faf9919f95667e62cd0
Julia2063 <sky206380@gmail.com.com>
2025-12-15 01:52:00 +0200

    Solution two

--- src/chainer.test.js
+++ src/chainer.test.js
@@ -19,4 +19,25 @@ describe('chainer', () => {
     expect(f).toHaveBeenCalledWith(8);
     expect(f2).toHaveBeenCalledWith(f(8));
   });
+
+  it('should return define value with real call', () => {
+    const f1 = jest.fn((x) => {
+      return x * 2;
+    });
+
+    const f2 = jest.fn((x) => {
+      return x + 2;
+    });
+
+    const f3 = jest.fn((x) => {
+      return Math.pow(x, 2);
+    });
+
+    const result = chainer([f1, f2, f3])(0);
+
+    expect(f1).toHaveBeenCalledWith(0);
+    expect(f2).toHaveBeenCalledWith(f1(0));
+    expect(f3).toHaveBeenCalledWith(f2(f1(0)));
+    expect(result).toBe(4);
+  });
 });
