878c3f0cdf81eca55aa028f8e6734184b4a0d792
Julia2063 <sky206380@gmail.com.com>
2025-12-15 01:38:14 +0200

    Solution

--- src/chainer.test.js
+++ src/chainer.test.js
@@ -1,3 +1,22 @@
-'use strict'
+'use strict';
 
 const { chainer } = require('./chainer');
+
+describe('chainer', () => {
+  it(`should call first function in arg array with input like arg`, () => {
+    const f = jest.fn();
+
+    chainer([f])(8);
+    expect(f).toHaveBeenCalledWith(8);
+  });
+
+  it(`should call second function in arg array
+     with first fnction in this array like arg`, () => {
+    const f = jest.fn();
+    const f2 = jest.fn();
+
+    chainer([f, f2])(8);
+    expect(f).toHaveBeenCalledWith(8);
+    expect(f2).toHaveBeenCalledWith(f(8));
+  });
+});
